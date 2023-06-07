d7eefdc075f3ddccf6f2e31360bc2a9e9216cc15
EF0601 <edwardfang0601@gmail.com>
2023-06-07 07:00:31 -0400

    v0.9.2-beta

---
+++ main.js
@@ -0,0 +1,93 @@
+let stats = {
+  tables: 0,
+  nails: 10,
+  wood: 10,
+  money: 50,
+};
+
+let statsDis = {
+  tablesDis: document.querySelector('.tableDis'),
+  nailsDis: document.querySelector('.nailsDis'),
+  woodDis: document.querySelector('.woodDis'),
+  moneyDis: document.querySelector('.moneyDis'),
+};
+
+function updateDis() {
+  statsDis.tablesDis.textContent = stats.tables;
+  statsDis.nailsDis.textContent = stats.nails;
+  statsDis.woodDis.textContent = stats.wood;
+  statsDis.moneyDis.textContent = stats.money;
+}
+
+//workbench randomizing
+function buildTable() {
+  if (document.querySelector('.tableInput').value.toUpperCase() == randomTable) {
+    if(stats.nails >= 4 && stats.wood >= 2){
+      stats.tables++;
+      stats.nails = stats.nails - 4;
+      stats.wood = stats.wood - 2;
+      randomizeTable();
+      console.log('User successfully built a table.');
+      document.querySelector('.tableInput').value = "";
+    }
+    console.log('User inputted correct sequence, but did not have materials.');
+  }
+  updateDis();
+}
+
+//workbench randomize function
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+let randomTable = "";
+function randomizeTable() {
+  let currentRandom = [];
+  for (let i = 0; i < 5; i++) {
+    currentRandom.push(alphabet[Math.floor(Math.random() * 26)]);
+  }
+  randomTable = currentRandom.join("");
+  document.querySelector('.letters').textContent = randomTable;
+  console.log(randomTable);
+}
+
+//purchasing
+function buyNails(amount) {
+  switch (amount) {
+    case 5:
+      if (stats.money >= 1) {
+        stats.nails = stats.nails + 5;
+        stats.money--;
+      }
+      break;
+    case 10:
+      if (stats.money >= 2) {
+        stats.nails = stats.nails + 10;
+        stats.money = stats.money - 2;
+      }
+      break;
+    default:
+      break;
+  }
+  updateDis();
+}
+
+function buyWood(amount) {
+  switch (amount) {
+    case 5:
+      if (stats.money >= 3) {
+        stats.wood = stats.wood + 5;
+        stats.money = stats.money - 3;
+      }
+      break;
+    case 10:
+      if (stats.money >= 6) {
+        stats.wood = stats.wood + 10;
+        stats.money = stats.money - 6;
+      }
+      break;
+    default:
+      break;
+  }
+  updateDis();
+}
+
+//on startup
+randomizeTable();
---
+++ tabs.js
@@ -0,0 +1,22 @@
+function openTab(item) {
+  switch (item) {
+    case 1:
+      document.querySelector('.tabContent1').style.display = "block";
+      document.querySelector('.tabContent2').style.display = "none";
+      document.querySelector('.tabContent4').style.display = "none";
+      break;
+    case 2:
+      document.querySelector('.tabContent2').style.display = "block";
+      document.querySelector('.tabContent1').style.display = "none";
+      document.querySelector('.tabContent4').style.display = "none";
+      break;
+    case 4:
+      document.querySelector('.tabContent4').style.display = "block";
+      document.querySelector('.tabContent2').style.display = "none";
+      document.querySelector('.tabContent1').style.display = "none";
+    default:
+      console.log('error');
+      break;
+  }
+}
+openTab(1);
